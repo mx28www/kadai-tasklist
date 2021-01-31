d49da4593e4340f098fdb75bb1ef6ab53a4d03b5
M.T <mx28www.x@gmail.com>
2021-01-31 07:19:02 +0000

    column

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -45,7 +45,14 @@ class TaskController extends Controller
      */
     public function store(Request $request)
     {
+        
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+            ]);
+            
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
@@ -95,8 +102,14 @@ class TaskController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $task = Task::findOrFail($id);
         
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+            ]);
+            
+        $task = Task::findOrFail($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
